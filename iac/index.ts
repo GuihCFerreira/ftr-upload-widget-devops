ea926c469ad5c0b23b6306419f9cc526f0f396d0
Guilherme de Carvalho Ferreira <guihhh231@gmail.com>
2026-04-17 23:42:17 -0300

    chore: removing unecessary resource

--- iac/index.ts
+++ iac/index.ts
@@ -6,12 +6,6 @@ const firstBucket = new aws.s3.Bucket("primeiro-bucket", {
   },
 });
 
-const secondBucket = new aws.s3.Bucket("segundo-bucket", {
-  tags: {
-    IAC: "true",
-  },
-});
-
 const ecr = new aws.ecr.Repository("primeiro-ecr", {
   name: "primeiro-teste-pos-ftr-widget-server",
   imageTagMutability: "IMMUTABLE",
@@ -23,10 +17,6 @@ const ecr = new aws.ecr.Repository("primeiro-ecr", {
 export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
-
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
