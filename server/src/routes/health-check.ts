ccbd84b1519d9316a2569b80eb9c704bee196895
Guilherme de Carvalho Ferreira <guihhh231@gmail.com>
2026-04-27 22:17:06 -0300

    test: change health check message to re run actions

--- server/src/routes/health-check.ts
+++ server/src/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get("/health", async (_request, reply) => {
-    return reply.status(200).send({ status: "OK" });
+    return reply.status(200).send({ status: "OK!" });
   });
 }
